30914749f2e34d44effb9527a771aa195fd7064f
pyk129 <pyk129@naver.com>
2023-05-12 17:30:08 +0900

    init

---
+++ app/src/main/java/com/shinhan/androidsample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.shinhan.androidsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
